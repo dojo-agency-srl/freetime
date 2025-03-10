8768127e501fa786c320a3e5eab5e00c692f0df1
Paolo Bassini <info@dojoagency.it>
2025-03-10 11:06:28 +0100

    aggiunta sendinblue

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import axios from "axios";
 import { Link } from "react-router-dom";
 
 import logo from "images/logo.png";
@@ -11,6 +13,66 @@ import { routes } from "routes.js";
 
 const Footer = () => {
 	const [scrolled, setScrolled] = useState(false);
+	const [responseMessage, setResponseMessage] = useState("");
+	const [isAccepted, setIsAccepted] = useState(false);
+
+	const handleCheckboxChange = (event) => {
+		setIsAccepted(event.target.checked);
+	};
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
+
+	const onSubmit = async (inputData) => {
+		const data = {
+			email: inputData.email,
+			attributes: {
+				email: "",
+			},
+			listIds: [0],
+		};
+
+		try {
+			const isInResponse = await fetch(
+				"https://api.brevo.com/v3/contacts/" + encodeURIComponent(data.email),
+				{
+					method: "GET",
+					headers: {
+						Accept: "application/json",
+						"api-key": "test",
+					},
+				}
+			);
+
+			debugger;
+
+			if (isInResponse.status === 200) {
+				setResponseMessage("Sei già iscritto alla newsletter");
+			} else if (isInResponse.status === 404) {
+				try {
+					const response = await axios.post("https://api.brevo.com/v3/contacts", data, {
+						headers: {
+							"Content-Type": "application/json",
+							"api-key": "test",
+						},
+					});
+
+					setResponseMessage(
+						"La tua iscrizione alla newsletter è avvenuta correttamente"
+					);
+				} catch (error) {
+					setResponseMessage("Ops, qualcosa è andato storto. Riprova più tardi");
+				}
+			} else {
+				setResponseMessage("Ops, qualcosa è andato storto. Riprova più tardi");
+			}
+		} catch (error) {
+			setResponseMessage("Ops, qualcosa è andato storto. Riprova più tardi");
+		}
+	};
 
 	useEffect(() => {
 		const handleScroll = () => {
@@ -77,10 +139,18 @@ const Footer = () => {
 									</ul>
 								</nav>
 
-								{/* <div className="newsletter_container">
-									<form action="#" id="newsletter_form" className="newsletter_form">
+								<div className="newsletter_container" style={{ display: "block" }}>
+									<form
+										id="newsletter_form"
+										className="newsletter_form"
+										onSubmit={handleSubmit(onSubmit)}>
 										<div class="acceptance-container">
-											<input type="checkbox" id="acceptance-checkbox" class="acceptance-checkbox" />
+											<input
+												type="checkbox"
+												id="acceptance-checkbox"
+												class="acceptance-checkbox"
+												onChange={handleCheckboxChange}
+											/>
 											<label for="acceptance-checkbox" class="acceptance-label">
 												<div>
 													Letta la <Link to="">Privacy Policy</Link>, presto il mio consenso per l’invio
@@ -90,13 +160,30 @@ const Footer = () => {
 										</div>
 										<input
 											type="text"
+											{...register("email", {
+												required: "L'email è obbligatoria",
+												pattern: {
+													value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+													message: "Inserisci un'email valida",
+												},
+											})}
 											className="newsletter_input"
 											placeholder={footer.inputPlaceholder}
-											required="required"
 										/>
-										<button className="newsletter_button">{footer.cta}</button>
-									</form> 
-								</div>*/}
+
+										<button
+											type="submit"
+											className="newsletter_button"
+											style={{
+												pointerEvents: isAccepted ? "auto" : "none",
+												opacity: isAccepted ? 1 : 0.2,
+											}}>
+											{footer.cta}
+										</button>
+									</form>
+								</div>
+								{errors.email && <p>{errors.email.message}</p>}
+								{responseMessage && <p style={{ color: "#fff" }}>{responseMessage}</p>}
 								<div className="copyright d-flex flex-row align-items-start justify-content-center">
 									<div style={{ lineHeight: 1.5 }}>
 										Copyright &copy;{new Date().getFullYear()} | All rights reserved | Made by{" "}
@@ -105,7 +192,7 @@ const Footer = () => {
 										</a>
 									</div>
 								</div>
-								<a href="https://colorlib.com" target="_blank" style={{ display: "none" }}>
+								<a href="https://colorlib.com" target="_blank" style={{ opacity: "0" }}>
 									Colorlib
 								</a>
 							</div>
